4b5f7dc8bd44499987d527a5eae7d2dcbf9f3167
dylandforbes <dylandforbes@gmail.com>
2016-11-04 15:35:11 -0500

    split off port from game.js

--- static/game.js
+++ static/game.js
@@ -1,6 +1,6 @@
 /* GLOBAL VARS */
 
-var puzzleServerURL = 'cmc307-01.mathcs.carleton.edu:5001';
+import 'host.js'
 
 var vehicleColor = '#306aad';
 var vipColor = '#b54141';
---
+++ static/host.js
@@ -0,0 +1,1 @@
+var puzzleServerURL = 'cmc307-01.mathcs.carleton.edu:5003';
