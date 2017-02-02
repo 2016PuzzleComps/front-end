ccce9bd78db3c630324318723218b5e76d1059bf
dylandforbes <dylandforbes@gmail.com>
2017-02-02 13:45:40 -0600

    make events non-passive

--- static/game.js
+++ static/game.js
@@ -544,12 +544,12 @@ document.body.addEventListener("touchstart", function (e) {
 	if (e.target == canvas) {
 		e.preventDefault();
 	}
-}, false);
+}, { passive: false}, false);
 document.body.addEventListener("touchend", function (e) {
 	if (e.target == canvas) {
 		e.preventDefault();
 	}
-}, false);
+}, { passive: false}, false);
 document.body.addEventListener("touchmove", function (e) {
 	if (e.target == canvas) {
 		e.preventDefault();
