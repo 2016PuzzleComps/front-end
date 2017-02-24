4e42a60912708090a64ff61fa142aaeebe84cd51
dylandforbes <dylandforbes@gmail.com>
2017-02-24 16:45:28 -0600

    demonstrate some problems in the test area of mle.py

--- mle.py
+++ mle.py
@@ -5,11 +5,10 @@ import sys
 
 # maximum likelihood estimator
 class MLE:
-    max_angle = 1000
-
     def __init__(self, max_score, norm_spread):
         self.norm_spread = norm_spread
         self.max_score = max_score
+        self.max_angle = 1000
 
     def expected_s(self, p, t, angle):
         return self.max_score/(1 + e**((t-p)/angle)) 
@@ -24,20 +23,25 @@ class MLE:
     @staticmethod
     def function_to_minimize(x, mle, ss, ps):
         t, angle = x
-        if angle > MLE.max_angle:
+        if angle > mle.max_angle:
             return sys.maxsize
         ret = 1.0
         for s, p in zip(ss, ps):
             ret *= mle.conditional_pdf(s, p, t, angle)
+        # print(x[0], end="   ")
+        # print(x[1], end="   ")
+        # print(-ret)
         return -ret
 
     def update(self, old_x, solve_scores, puzzle_scores):
         result = minimize(self.function_to_minimize, old_x, args=(self, solve_scores, puzzle_scores), method='Nelder-Mead')
-        print(result)
         return result.x
 
 
 if __name__ == '__main__':
     m = MLE(1259.77, 584.3712)
-    N = 10
-    t, angle = m.update((500, 100), [100 for i in range(N)], [600 for i in range(N)])
+    # this blows up
+    print(m.update((550, 68), [500, 600], [100, 105]))
+    # but these don't
+    print(m.update((550, 67), [500, 600], [100, 105])) # 68 --> 67
+    print(m.update((550, 500), [500, 600], [100, 100])) # 105 --> 100
