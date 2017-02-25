cf6249f1facd5fb072eb6d8ec2e09104b5f9ea9d
dylandforbes <dylandforbes@gmail.com>
2017-02-25 10:42:10 -0600

    try ideal score = 700

--- puzzle_server.py
+++ puzzle_server.py
@@ -81,7 +81,7 @@ def post_log_file():
 solvers_table = {}
 
 # ideal score
-ideal_score = 500
+ideal_score = 700
 max_score = 1259.77 # Highest value in db. wwl=279.1248
 norm_spread = 584.3712 # Average standard deviation of mturk data
 angle = 300
