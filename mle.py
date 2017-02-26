81508c4f773b786d9556a041184f7a6586e66075
dylandforbes <dylandforbes@gmail.com>
2017-02-26 10:12:13 -0600

    mess around with mle.py more

--- mle.py
+++ mle.py
@@ -28,11 +28,16 @@ class MLE:
 
     def get_new_true_skill(self, old_t, solve_scores, puzzle_scores):
         result = minimize(self.function_to_minimize, old_t, args=(self, solve_scores, puzzle_scores), method='Nelder-Mead')
-        print(result)
         return result.x[0]
 
 
 if __name__ == '__main__':
-    m = MLE(1000, 70)
-    N = 10
-    result = m.get_new_true_skill(500, [200 for i in range(N)], [500 for i in range(N)])
+    angle = 300
+    ideal_score = 90
+    mle = MLE(209, 94, angle)
+    # starting puzzle
+    starting_puzzle_diff = mle.expected_p(ideal_score, ideal_score)
+    print(starting_puzzle_diff)
+    # solve it in 15 moves
+    new_skill = mle.get_new_true_skill(ideal_score, [starting_puzzle_diff/3], [starting_puzzle_diff])
+    print(new_skill)
